72d86e74418687e48d196d7e27d6ead2bc0234c7
NgoPhuocLoi <phuocloi11223@gmail.com>
2024-04-02 14:24:32 +0700

    Modify script to use bun in project

--- tmux-project.sh
+++ tmux-project.sh
@@ -6,15 +6,15 @@ tmux new-session -d -s $session
 
 tmux rename-window -t $session:0 'frontend'
 
-tmux send-keys -t $session:0 'cd ~/projects/clothing-store/frontend' C-m 'clear && npm run dev' C-m 
+tmux send-keys -t $session:0 'cd ~/projects/clothing-store/frontend' C-m 'clear && bun dev' C-m 
 
 tmux new-window -t $session:1 -n 'backend'
 
-tmux send-keys -t $session:1 'cd ~/projects/clothing-store/backend' C-m 'clear && npm run dev' C-m 
+tmux send-keys -t $session:1 'cd ~/projects/clothing-store/backend' C-m 'clear && bun dev' C-m 
 
 tmux new-window -t $session:2 -n 'admin'
 
-tmux send-keys -t $session:2 'cd ~/projects/clothing-store/admin' C-m 'clear && npm run dev' C-m 
+tmux send-keys -t $session:2 'cd ~/projects/clothing-store/admin' C-m 'clear && bun dev' C-m 
 
 tmux new-window -t $session:3 -n 'monitor'
 
